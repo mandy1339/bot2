430240e5661af8bdd6e6b54733913470cff92fd9
DoraEx <skinnycarrot@gmail.com>
2017-09-16 20:27:00 -0400

    Initial commit

---
+++ ramhackbot/app.js
@@ -0,0 +1,24 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+require('dotenv').config();
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    //appId: process.env.SLACK_APP_ID,
+    //appPassword: process.env.SLACK_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    //Default dialog
+    session.send("Hey, whats up?");
+});
